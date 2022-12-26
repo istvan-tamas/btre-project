7f5a66711a9a501f085a911829503410eb2efcf0
istvan-tamas <istvan.tamas90@gmail.com>
2022-12-26 19:34:30 +0100

    Added realtors, and customized the admin area with CSS

--- btre/settings.py
+++ btre/settings.py
@@ -132,3 +132,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Media files location
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
