2554166d74c439f6f19803cfc069efaab8f7c588
istvan-tamas <istvan.tamas90@gmail.com>
2023-01-11 22:24:40 +0100

    Added fields for the listing page

--- listings/views.py
+++ listings/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .models import Listing
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
@@ -10,8 +10,6 @@ def index(request):
     page = request.GET.get('page')
     paged_listings = paginator.get_page(page)
 
-
-
     context = {
         'listings': paged_listings
     }
@@ -19,7 +17,13 @@ def index(request):
     return render(request, 'listings/listings.html',context)
 
 def listing(request, listing_id):
-    return render(request, 'listings/listing.html')
+    listing = get_object_or_404(Listing, pk=listing_id)
+
+    context = {
+        'listing' : listing
+    }
+
+    return render(request, 'listings/listing.html', context)
 
 def search(request):
     return render(request, 'listings/search.html')
