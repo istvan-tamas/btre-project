676184566e71af463ab9fc5b6e7c46435ddf06d9
istvan-tamas <istvan.tamas90@gmail.com>
2023-02-02 17:39:07 +0100

    Added messaging to the form submission

--- btre/settings.py
+++ btre/settings.py
@@ -139,3 +139,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger'
+}
