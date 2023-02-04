adf601af91f703c0072afcf18de9f5d3ab9afdb7
istvan-tamas <istvan.tamas90@gmail.com>
2023-02-04 14:25:17 +0100

    Added logig to the registration form

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,32 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-
+from django.contrib.auth.models import User
 # Create your views here.
 def register(request):
     if request.method == 'POST':
-        messages.error(request, 'Testing')
-        return redirect('register')
+        # Get form values
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        if password == password2:
+            if User.objects.filter(username=username).exists():
+                messages.error(request, 'Username alredy exists')
+                return redirect('register')
+            else:
+                if User.objects.filter(email=email).exists():
+                messages.error(request, 'Email already exists')
+                return redirect('register')
+        
+            else:
+                user = User.objects.create_user(username=username, password=password, email=email, first_name=first_name, last_name=last_name)
+        
+        else:
+            messages.error(request, 'Passwords do not match')
+            return redirect('register')
     else:
         return render(request, 'accounts/register.html')
 
