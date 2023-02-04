83941892a6d8702e75c4031801e3c167e01b544e
istvan-tamas <istvan.tamas90@gmail.com>
2023-02-04 14:29:12 +0100

    Fixed identation problem in accounts/views.py

--- accounts/views.py
+++ accounts/views.py
@@ -16,14 +16,11 @@ def register(request):
             if User.objects.filter(username=username).exists():
                 messages.error(request, 'Username alredy exists')
                 return redirect('register')
-            else:
-                if User.objects.filter(email=email).exists():
-                messages.error(request, 'Email already exists')
-                return redirect('register')
-        
+            elif User.objects.filter(email=email).exists():
+                    messages.error(request, 'Email already exists')
+                    return redirect('register')
             else:
                 user = User.objects.create_user(username=username, password=password, email=email, first_name=first_name, last_name=last_name)
-        
         else:
             messages.error(request, 'Passwords do not match')
             return redirect('register')
