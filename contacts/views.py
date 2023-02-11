9af93b3d9099e5980060afec20f913677dd0f6c0
istvan-tamas <istvan.tamas90@gmail.com>
2023-02-11 16:00:54 +0100

    Added email notification for inquries

--- contacts/views.py
+++ contacts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from  .models import Contact
 from django.contrib import messages
+from django.core.mail import send_mail
 
 # Create your views here.
 def contact(request):
@@ -12,11 +13,25 @@ def contact(request):
         phone = request.POST['phone']
         message = request.POST['message']
         user_id = request.POST['user_id']
-        realtor_email = request.POST['realtor_email']        
+        realtor_email = request.POST['realtor_email']   
+
+        if request.user.is_authenticated:
+            user_id = request.user.id
+            has_contacted = Contact.objects.all().filter(listing_id=listing_id, user_id = user_id)
+            if has_contacted:
+                messages.error(request, 'Inqury already made')
+                redirect('/listings'+listing_id)     
 
         contact = Contact(listing=listing, listing_id=listing_id, name = name, email = email, phone = phone, message = message, user_id = user_id)
 
         contact.save()
+
+        send_mail(
+            'Property Listing Inqury',
+            'There has benn an inqury for ' + listing + '. Sign into the admin panel',
+            [realtor_email],
+            fail_silently=False
+        )
 
         messages.success(request, 'Submission successful')
         return redirect('/listings/'+listing_id)
